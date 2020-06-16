f32d21420b431600c6adb2177759064853bff8b0
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-06-16 11:03:45 +0100

    Update deps, remove unused

--- etc/rollup.config.js
+++ etc/rollup.config.js
@@ -4,7 +4,6 @@ const commonjs = require('@rollup/plugin-commonjs')
 module.exports = [{
   input: 'app/_javascripts/application.js',
   output: {
-    legacy: true,
     file: 'public/javascripts/application.js',
     format: 'iife'
   },
