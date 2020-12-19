2fbc5114ec5dc400152714bb794ca0e26471f4db
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-19 21:48:09 +0000

    Lint .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -9,15 +9,15 @@ module.exports = function (eleventyConfig) {
     serveStaticOptions: {
       extensions: ['html']
     }
-  });
+  })
 
   // Template libraries
-  eleventyConfig.setLibrary('njk', require('./lib/libraries/nunjucks'));
-  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown'));
+  eleventyConfig.setLibrary('njk', require('./lib/libraries/nunjucks'))
+  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown'))
 
   // Plugins
-  eleventyConfig.addPlugin(require('@11ty/eleventy-navigation'));
-  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'));
+  eleventyConfig.addPlugin(require('@11ty/eleventy-navigation'))
+  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'))
 
   // Filters
   eleventyConfig.addFilter('date', require('./lib/filters/date'))
@@ -36,14 +36,14 @@ module.exports = function (eleventyConfig) {
   // Collections
 
   // Passthrough
-  eleventyConfig.addPassthroughCopy('./app/documents');
-  eleventyConfig.addPassthroughCopy('./app/images');
+  eleventyConfig.addPassthroughCopy('./app/documents')
+  eleventyConfig.addPassthroughCopy('./app/images')
   eleventyConfig.addPassthroughCopy({
     'node_modules/govuk-frontend/govuk/assets': 'assets'
-  });
+  })
 
   // Enable data deep merge
-  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true)
 
   // Config
   return {
@@ -58,5 +58,5 @@ module.exports = function (eleventyConfig) {
     },
     templateFormats: ['njk', 'md'],
     passthroughFileCopy: true
-  };
-};
+  }
+}
