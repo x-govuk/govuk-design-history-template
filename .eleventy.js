4cdab2b2c8b40231e43e270a4b4cf56d271e2751
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-12-27 19:13:59 +0000

    Use govuk-eleventy-plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,9 @@
 module.exports = function (eleventyConfig) {
-  // Browser Sync
-  eleventyConfig.setBrowserSyncConfig({
-    rewriteRules: [{
-      match: /\/image\/(\d+)(x)?(\d+)?/g,
-      replace: '/images'
-    }],
-    serveStatic: ['public'],
-    serveStaticOptions: {
-      extensions: ['html']
-    }
-  })
-
-  // Template libraries
-  eleventyConfig.setLibrary('njk', require('./lib/libraries/nunjucks'))
-  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown'))
-
   // Plugins
-  eleventyConfig.addPlugin(require('@11ty/eleventy-navigation'))
-  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'))
-
-  // Filters
-  eleventyConfig.addFilter('date', require('./lib/filters/date'))
-  eleventyConfig.addFilter('markdown', require('./lib/filters/markdown'))
-  eleventyConfig.addFilter('pretty', require('./lib/filters/pretty'))
-  eleventyConfig.addFilter('tokenize', require('./lib/filters/tokenize'))
-  eleventyConfig.addFilter('widont', require('./lib/filters/widont'))
-
-  // Transforms
-
-  // Collections
+  eleventyConfig.addPlugin(require('govuk-eleventy-plugin'), {
+    searchIndex: '/search.json',
+    views: ['app/_components']
+  })
 
   // Passthrough
   eleventyConfig.addPassthroughCopy({ './app/images': '.' })
@@ -45,6 +20,6 @@ module.exports = function (eleventyConfig) {
       layouts: '_layouts',
       includes: '_components'
     },
-    templateFormats: ['njk', 'md', '11ty.js']
+    templateFormats: ['njk', 'md']
   }
 }
--- app/_javascripts/application.js
+++
@@ -1,9 +0,0 @@
-import Search from '../_components/site-search/_site-search.js'
-
-const $searchContainer = document.querySelector('[data-module="app-search"]')
-new Search($searchContainer).init()
-
-window.onload = function () {
-  document.documentElement.classList.remove('no-js')
-  window.GOVUKFrontend.initAll()
-}
