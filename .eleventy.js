a676780a2c25ce8b8c66091fab7c69a8c5fd6f42
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-10-14 14:57:22 +0100

    Remove some GOV.UK branding from example site

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,25 @@
 module.exports = function (eleventyConfig) {
-  // Plugins
+  // Options to customise the appearance of your design history
+  // https://x-govuk.github.io/govuk-eleventy-plugin/options/
   eleventyConfig.addPlugin(require('govuk-eleventy-plugin'), {
     stylesheets: [
       '/styles/application.css'
     ],
     headingPermalinks: true,
     header: {
+      organisationLogo: false,
       productName: 'Design history',
       search: {
         indexPath: '/search.json',
         sitemapPath: '/sitemap'
+      }
+    },
+    footer: {
+      contentLicence: {
+        html: 'An unofficial community project. <a class="govuk-footer__link" href="https://github.com/x-govuk/x-govuk.github.io">GitHub source</a>.'
+      },
+      copyright: {
+        text: '© X-GOVUK'
       }
     }
   })
