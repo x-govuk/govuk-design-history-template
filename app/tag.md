2bc18a18bc153d60a29aa5c7b2b46e513688084b
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-08-26 23:33:37 +0100

    Update govuk-eleventy-plugin dependency

--- app/tag.md
+++
@@ -1,9 +0,0 @@
----
-layout: tag
-pagination:
-  data: collections.tags
-  size: 1
-  alias: tag
-permalink: '/tags/{{ tag | slug }}/'
-eleventyExcludeFromCollections: true
----
--- eleventy.config.js
+++ eleventy.config.js
@@ -4,16 +4,20 @@ export default function (eleventyConfig) {
   // Options to customise the appearance of your design history
   // https://x-govuk.github.io/govuk-eleventy-plugin/options/
   eleventyConfig.addPlugin(govukEleventyPlugin, {
-    stylesheets: [
-      '/styles/application.css'
-    ],
-    headingPermalinks: true,
     header: {
       productName: 'Design History',
       search: {
-        indexPath: '/search.json',
+        indexPath: '/search-index.json',
         sitemapPath: '/sitemap'
       }
+    },
+    headingPermalinks: true,
+    stylesheets: [
+      '/styles/application.css'
+    ],
+    templates: {
+      searchIndex: true,
+      tags: true
     },
     url:
       process.env.GITHUB_ACTIONS &&
